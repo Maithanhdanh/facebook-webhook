4d8cb76e5a7e1f2fcc3bf0d50af32b4553e803d4
kalihidroxit <kalihidroxit@gmail.com>
2021-12-07 22:10:08 +0700

    rearange file

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export enum loanModOptions {
+  FIX = 'Fix my interest rate',
+  REFIX = 'Refix my interest rate',
+  SPLIT = 'Split my home loan',
+  YES = 'Yes',
+  NO = `No, I want to pay more, I'm rich`
+}
