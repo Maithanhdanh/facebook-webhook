f6e0403964a1806c5e2b112f21d047cbee82c634
Dantis <mtd.maithanhdanh@gmail.com>
2021-12-07 14:58:48 +0700

    init BootBot

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,75 @@
-import * as moduleAlias from 'module-alias';
-const sourcePath = process.env.NODE_ENV === 'development' ? 'src' : 'build';
-moduleAlias.addAliases({
-  '@server': sourcePath,
-  '@config': `${sourcePath}/config`,
-  '@controller': `${sourcePath}/controller`,
+import BootBot from 'bootbot';
+
+const bot = new BootBot({
+  accessToken: process.env.PAGE_ACCESS_TOKEN,
+  verifyToken: process.env.VERIFY_TOKEN,
+  appSecret: process.env.APP_SECRET,
 });
 
-import { createServer } from '@config/express';
-import { AddressInfo } from 'net';
-import http from 'http';
-import { logger } from '@config/logger';
-
-const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || '5000';
-
-async function startServer() {
-  const app = createServer();
-  const server = http.createServer(app).listen({ host, port }, () => {
-    const addressInfo = server.address() as AddressInfo;
-    logger.info(
-      `Server ready at http://${addressInfo.address}:${addressInfo.port}`,
-    );
+bot.hear(['hello', 'hi', /hey( there)?/i], (_payload, chat) => {
+  // Send a text message followed by another text message that contains a typing indicator
+  chat.say('Hello, human friend!').then(() => {
+      chat.say('How are you today?', { typing: true });
   });
+});
 
-  const signalTraps: NodeJS.Signals[] = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
-  signalTraps.forEach((type) => {
-    process.once(type, async () => {
-      logger.info(`process.once ${type}`);
+bot.hear(['food', 'hungry'], (_payload, chat) => {
+  // Send a text message with quick replies
+  chat.say({
+      text: 'What do you want to eat today?',
+      quickReplies: ['Mexican', 'Italian', 'American', 'Argentine']
+  });
+});
 
-      server.close(() => {
-        logger.debug('HTTP server closed');
+bot.hear(['help'], (_payload, chat) => {
+  // Send a text message with buttons
+  chat.say({
+      text: 'What do you need help with?',
+      buttons: [
+          { type: 'postback', title: 'Settings', payload: 'HELP_SETTINGS' },
+          { type: 'postback', title: 'FAQ', payload: 'HELP_FAQ' },
+          { type: 'postback', title: 'Talk to a human', payload: 'HELP_HUMAN' }
+      ]
+  });
+});
+
+bot.hear('image', (_payload, chat) => {
+  // Send an attachment
+  chat.say({
+      attachment: 'image',
+      url: 'http://example.com/image.png'
+  });
+});
+
+bot.hear('ask me something', (_payload, chat) => {
+ 
+  const askName = (convo) => {
+      convo.ask(`What's your name?`, (payload, convo) => {
+          const text = payload.message.text;
+          convo.set('name', text);
+          convo.say(`Oh, your name is ${text}`).then(() => askFavoriteFood(convo));
       });
-    });
-  });
-}
+  };
 
-startServer();
+  const askFavoriteFood = (convo) => {
+      convo.ask(`What's your favorite food?`, (payload, convo) => {
+          const text = payload.message.text;
+          convo.set('food', text);
+          convo.say(`Got it, your favorite food is ${text}`).then(() => sendSummary(convo));
+      });
+  };
+
+  const sendSummary = (convo) => {
+      convo.say(`Ok, here's what you told me about you:
+        - Name: ${convo.get('name')}
+        - Favorite Food: ${convo.get('food')}`);
+    convo.end();
+  };
+
+  chat.conversation((convo) => {
+      askName(convo);
+  });
+});
+
+
+bot.start();
