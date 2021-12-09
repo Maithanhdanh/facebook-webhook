df7781d033751e6c1b011e06ce271f76e6b2e5ef
kalihidroxit <kalihidroxit@gmail.com>
2021-12-09 20:24:03 +0700

    test time

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -30,7 +30,6 @@ export const initWebRoutes = (bot)=> {
   router.get("/confirm/:id/:timestamp", (_req, res) => {
     const current = new Date()
     current.setSeconds(current.getSeconds() - 100);
-    console.log('________________________', _req.params)
     console.log('________________________', current.getTime());
     console.log('________________________', _req.params.timestamp)
     if (Number(_req.params.timestamp) < current.getTime()) {
